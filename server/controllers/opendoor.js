0eee5e93de1933fc64069bdb0824330559a04a0b
liyang <liyang@ynu.edu.cn>
2018-06-01 10:27:04 +0800

    update config

--- server/controllers/opendoor.js
+++ server/controllers/opendoor.js
@@ -1,13 +1,30 @@
 const fetch = require('node-fetch');
+const fs = require('fs')
+const path = require('path')
+const { mysql: config, backend_opendoor_host } = require('../config')
+const knex = require('knex')({
+  client: 'mysql',
+  connection: {
+    host: config.host,
+    port: config.port,
+    user: config.user,
+    password: config.pass,
+    database: config.db,
+    charset: config.char,
+    multipleStatements: true
+  }
+})
 
-async function get(ctx, next) {
+async function post(ctx, next) {
   // ctx.body = { message: 'ok' };
   // 执行开门的UDP请求
   try {
-    const response = await fetch('http://opendoor.ynu.test.ynu.edu.cn/opendoor?door=1');
+    const response = await fetch(`${backend_opendoor_host}?door=${ctx.request.body.id}`);
     const responseJson = await response.json();
+    
     if (responseJson.message == "successful") {
       // 设置返回的内容
+      await knex('log').insert({ openid: ctx.request.body.openid, door: ctx.request.body.id})
       console.info({ message: 'ok', data: responseJson.data });
       ctx.body = { message: 'ok', data: responseJson.data };
     }
@@ -23,5 +40,5 @@ async function get(ctx, next) {
 }
 
 module.exports = {
-  get
+  post
 }
